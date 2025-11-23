56744e769f1e86dd0513e26a53ecc5a3bf7aed83
GitChan0456 <syc7179@naver.com>
2025-11-23 23:13:36 +0900

    SQLite 연동(테이블 관리 UserDBHelper.java 생성)

---
+++ app/src/main/java/com/example/socialmymap/UserDBHelper.java
@@ -0,0 +1,37 @@
+package com.example.socialmymap;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "socialmap.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "user_table";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_NICKNAME = "nickname";
+
+    public UserDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_USERS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_EMAIL + " TEXT UNIQUE, "
+                + COLUMN_PASSWORD + " TEXT, "
+                + COLUMN_NICKNAME + " TEXT" + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+}
